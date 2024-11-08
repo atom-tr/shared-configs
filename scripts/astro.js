ac40663014cea473741a80cb64e9ce2584717290
atom-tr <thai.tr@hotmail.com>
2024-11-08 11:38:33 +0700

    json not js for both python and nodejs

--- scripts/astro.js
+++ scripts/astro.js
@@ -2,28 +2,27 @@ const fs = require('fs');
 const path = require('path');
 
 // Load consts.js
-const constsPath = path.join(__dirname, '../consts.js');
-const consts = require(constsPath);
-console.log(consts)
+const cnf_path = path.join(__dirname, '../consts.json');
+const cnf = JSON.parse(fs.readFileSync(cnf_path, 'utf8'));
 
 // Prepare the content for astro.ts
-const astroContent = `
+const astro_content = `
 export const SITE = {
-  TITLE: '${consts.site.title}',
-  DESCRIPTION: '${consts.site.description}',
-  EMAIL: '${consts.email_address}',
-  NUM_POSTS_ON_HOMEPAGE: ${consts.site.num_posts_on_homepage},
-  POSTS_PER_PAGE: ${consts.site.posts_per_page},
-  SITEURL: '${consts.site.site_url}',
+  TITLE: '${cnf.site.title}',
+  DESCRIPTION: '${cnf.site.description}',
+  EMAIL: '${cnf.email_address}',
+  NUM_POSTS_ON_HOMEPAGE: ${cnf.site.num_posts_on_homepage},
+  POSTS_PER_PAGE: ${cnf.site.posts_per_page},
+  SITEURL: '${cnf.site.site_url}',
 };
 
 export const SOCIAL_LINKS = [
-  ...${JSON.stringify(consts.social, null, 2)},
+  ...${JSON.stringify(cnf.social, null, 2)},
   { href: '/rss.xml', label: 'RSS' }
 ];
 `;
 
 // Write to astro.ts in the shared-configs directory
 const outputPath = path.join(__dirname, '../astro.ts');
-fs.writeFileSync(outputPath, astroContent.trim());
+fs.writeFileSync(outputPath, astro_content.trim());
 console.log('astro.ts has been generated successfully in shared-configs.');
